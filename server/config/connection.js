f7ef53cb11f09277e7b70d583d212287119809c9
kurtriecken <138238647+kurtriecken@users.noreply.github.com>
2025-04-26 12:53:20 -0400

    test commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lovelink"
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lovelink",
+  {
+    ssl: true, // make sure SSL/TLS is used
+    tlsInsecure: false, // ensure secure TLS connection (optional, usually default)
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
 );
 
 module.exports = mongoose.connection;
