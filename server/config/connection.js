5afd64742b0995e369f32b053a0cc5a40c5a0eca
kurtriecken <138238647+kurtriecken@users.noreply.github.com>
2025-05-11 14:29:03 -0400

    feat(test-branch): adds dotenv package

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,18 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lovelink",
-  {
-    ssl: true, // make sure SSL/TLS is used
-    tlsInsecure: false, // ensure secure TLS connection (optional, usually default)
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+const connectionString = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/lovelink";
+
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}
+
+if (connectionString.startsWith('mongodb+srv://')) {
+  console.log('test ing 000')
+  mongooseOptions.ssl = true;
+  mongooseOptions.tlsInsecure = false;
+}
+
+mongoose.connect(connectionString, mongooseOptions)
 
 module.exports = mongoose.connection;
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const db = require('../config/connection');
 const { User } = require('../models');
 const cleanDB = require('./cleanDB');
